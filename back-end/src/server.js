72246822f7d4caa69f43b4b670a1d7abd6c62f1a
Eliecer Lemus <lemuselr@gmail.com>
2022-04-19 17:28:07 -0400

    [fix] code format commit

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const { json } = require('express');
 const morgan = require('morgan');
 const transactionRoutes = require('./routes/transaction.routes')
+const cors = require('cors')
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(json());
-
+app.use(cors())
 //routes
 
 app.use('/api/', transactionRoutes)
